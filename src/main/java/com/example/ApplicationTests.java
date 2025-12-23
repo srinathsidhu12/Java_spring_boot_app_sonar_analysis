ce151554cb32bdedd209a378bda9c8023b8c29b3
root <root@ip-172-31-38-161.ap-south-1.compute.internal>
2025-12-23 12:40:35 +0000

    modified ApplicationTests.java

--- src/main/java/com/example/ApplicationTests.java
+++ src/main/java/com/example/ApplicationTests.java
@@ -3,7 +3,7 @@ package com.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class AppTest {
+public class ApplicationTests {
 
     @Test
     void dummyTest() {
